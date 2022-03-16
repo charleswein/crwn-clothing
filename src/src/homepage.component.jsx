145e79e36183fa1f70da667c7844d93bb8c60fb9
Yury Dunets <86290237+ydunetsdev@users.noreply.github.com>
2022-03-16 17:30:36 +0300

    init: react app module

---
+++ src/src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">TROUSERS</h1>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">SNEAKERS</h1>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">MENS</h1>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <p className='subtitle'>Shop Now</p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage
